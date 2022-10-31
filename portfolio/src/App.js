427f5da19f4746ebe7426de7d79ebdddd086b7a9
Iler <ilerwatson22@gmail.com>
2022-10-31 18:48:24 +0000

    Removed guide routes once tested while setting up header and footer components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,50 +1,16 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/about" element={<About />}/>
-          <Route path="/users" element={<Users />}/>
-          <Route path="/" element={<Home />}/>
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <Header />
+      </Router>
+      <Footer />
+    </>
   );
 }
 
